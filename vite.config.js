f405317bbcaba11ed9120a49a3036602369619d1
DESKTOP-1CL0VCU\USER <henry@bizpro.com.tw>
2024-06-11 16:57:53 +0800

    deploy test

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
+// import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
+
+const isLib = process.env.BUILD_LIB === 'true';
+
 export default defineConfig({
+  base: "/ViteDemo",
   plugins: [
     vue(
       // treat all tags with a dash as custom elements
@@ -13,8 +20,10 @@ export default defineConfig({
             isCustomElement: (tag) => tag.includes('-')
           }
         }
-      }),
-
+      }
+    ),
+    // VitePWA(),
+    // VueDevTools(),
   ],
   define: {
     'process.env': {}
@@ -26,26 +35,33 @@ export default defineConfig({
     }
   },
   build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      // entry: resolve(__dirname, 'lib/main.js'),
-      entry: './src/main.ce.js',
-      name: 'CustomElements',
-      // the proper extensions will be added
-      fileName: (format) => `custom-elements.${format}.js`,
-    },
-    // rollupOptions: {
-    //   // make sure to externalize deps that shouldn't be bundled
-    //   // into your library
-    //   external: ['vue'],
-    //   output: {
-    //     // Provide global variables to use in the UMD build
-    //     // for externalized deps
-    //     globals: {
-    //       vue: 'Vue',
-    //     },
+    // ...(isLib ? {
+    //   lib: {
+    //     // Could also be a dictionary or array of multiple entry points
+    //     // entry: resolve(__dirname, 'lib/main.js'),
+    //     entry: './src/main.ce.js',
+    //     name: 'CustomElements',
+    //     // the proper extensions will be added
+    //     fileName: (format) => `custom-elements.${format}.js`,
     //   },
-    // },
+    //   // rollupOptions: {
+    //   //   // make sure to externalize deps that shouldn't be bundled
+    //   //   // into your library
+    //   //   external: ['vue'],
+    //   //   output: {
+    //   //     // Provide global variables to use in the UMD build
+    //   //     // for externalized deps
+    //   //     globals: {
+    //   //       vue: 'Vue',
+    //   //     },
+    //   //   },
+    //   // },
+    // } : {
+    //   outDir: 'dist-app', // 应用程序构建的输出目录
+    //   rollupOptions: {
+    //     input: 'index.html',
+    //   }
+    // })
   },
   server: {
     proxy: {
