b965cadc733f45f4c26cf2f0c1c01f1c257718be
Henry Wu <henrywu@bizpro.com.tw>
2025-04-01 09:09:50 +0800

    路徑變數測試

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv  } from 'vite'
 import vue from '@vitejs/plugin-vue'
 //import VueDevTools from 'vite-plugin-vue-devtools'
 
@@ -9,63 +9,56 @@ import vue from '@vitejs/plugin-vue'
 
 // const isLib = process.env.BUILD_LIB === 'true';
 
-export default defineConfig({
-  base: "/",
-  plugins: [
-    vue(
-      // treat all tags with a dash as custom elements
-      {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    base: env.VITE_BASE || '/',
+    plugins: [
+      vue({
         template: {
           compilerOptions: {
             isCustomElement: (tag) => tag.includes('-')
           }
         }
-      }
-    ),
-    // VitePWA(),
-    // VueDevTools(),
-  ],
-  define: {
-    'process.env': {}
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-
-    }
-  },
-  build: {
-    // lib: {
-    //   // Could also be a dictionary or array of multiple entry points
-    //   // entry: resolve(__dirname, 'lib/main.js'),
-    //   entry: './src/main.ce.js',
-    //   name: 'CustomElements',
-    //   // the proper extensions will be added
-    //   fileName: (format) => `custom-elements.${format}.js`,
-    // },
-    // rollupOptions: {
-    //   // make sure to externalize deps that shouldn't be bundled
-    //   // into your library
-    //   external: ['vue'],
-    //   output: {
-    //     // Provide global variables to use in the UMD build
-    //     // for externalized deps
-    //     globals: {
-    //       vue: 'Vue',
-    //     },
-    //   },
-    // },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    proxy: {
-      '/api': {
-        // target: 'https://openapi.twse.com.tw/',
-        target: 'https://api.notion.com/v1/pages/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+      }),
+      // VitePWA(),
+      // VueDevTools(),
+    ],
+    define: {
+      'process.env': {}
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
-  },
+    build: {
+      // lib: {
+      //   entry: './src/main.ce.js',
+      //   name: 'CustomElements',
+      //   fileName: (format) => `custom-elements.${format}.js`,
+      // },
+      // rollupOptions: {
+      //   external: ['vue'],
+      //   output: {
+      //     globals: {
+      //       vue: 'Vue',
+      //     },
+      //   },
+      // },
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+      proxy: {
+        '/api': {
+          // target: 'https://openapi.twse.com.tw/',
+          target: 'https://api.notion.com/v1/pages/',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      },
+    },
+  }
 })
