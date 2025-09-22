b3ef4de92dc72a80b90dc3985aab4d00baf85008
Henry Wu <henrywu@bizpro.com.tw>
2025-09-22 17:07:57 +0800

    結構調整

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -196,7 +196,7 @@ const loginWithFaceID = async (data) => {
       }
     );
     console.log("挑戰資料:");
-    const challengeData = JSON.parse(resp.data);
+    const challengeData = JSON.parse(resp.data.data);
     // 2️⃣ 呼叫 WebAuthn API
     const credential = await navigator.credentials.get({
       publicKey: {
